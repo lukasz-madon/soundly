32688b76ea23d52b91ace62469ef19d7810e8cca
lukas madon <lukas@firestartr.co>
2014-02-02 22:32:29 +0000

    indentation

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
 if os.environ.get("HEROKU") is not None:
-SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+	SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 else:
 	SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://dbuser:P@ssw0rd@localhost/localdb"
 GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
