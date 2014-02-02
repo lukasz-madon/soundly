55fd27e41567ad26ad0d1a7cc4532740196d9772
lukas madon <lukas@firestartr.co>
2014-02-02 22:30:44 +0000

    heroku db url

--- settings.py
+++ settings.py
@@ -2,9 +2,9 @@ import os
 
 SECRET_KEY = os.environ["FLASK_SECRET_KEY"]
 if os.environ.get("HEROKU") is not None:
-	SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://dbuser:P@ssw0rd@localhost/localdb"
+SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 else:
-	SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
+	SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://dbuser:P@ssw0rd@localhost/localdb"
 GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
 GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
 CSRF_ENABLED = True
