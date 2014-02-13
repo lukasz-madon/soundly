01451adc41b7893fc2d0e05bc9121f00f3ff2da5
lukas madon <lukas@firestartr.co>
2014-02-13 19:33:13 +0000

    uploading video to youtube

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import string
+import time
 from random import SystemRandom
 import subprocess as sp
 
@@ -11,7 +12,10 @@ from werkzeug.utils import secure_filename
 
 from simplekv.memory import DictStore
 import httplib2
+import httplib
 from apiclient.discovery import build
+from apiclient.http import MediaFileUpload
+from apiclient.errors import HttpError
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.client import AccessTokenRefreshError
 from oauth2client.client import flow_from_clientsecrets
@@ -31,6 +35,56 @@ KVSessionExtension(store, app)
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
 
+# Explicitly tell the underlying HTTP transport library not to retry, since
+# we are handling retry logic ourselves.
+httplib2.RETRIES = 1
+
+# Maximum number of times to retry before giving up.
+MAX_RETRIES = 10
+
+# Always retry when these exceptions are raised.
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
+  httplib.IncompleteRead, httplib.ImproperConnectionState,
+  httplib.CannotSendRequest, httplib.CannotSendHeader,
+  httplib.ResponseNotReady, httplib.BadStatusLine)
+
+# Always retry when an apiclient.errors.HttpError with one of these status
+# codes is raised.
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
+
+def resumable_upload(insert_request):
+  response = None
+  error = None
+  retry = 0
+  while response is None:
+    try:
+      app.logger.info("Uploading file...")
+      status, response = insert_request.next_chunk()
+      if "id" in response:
+        print "'%s' (video id: %s) was successfully uploaded." % (
+          options.title, response['id'])
+      else:
+        exit("The upload failed with an unexpected response: %s" % response)
+    except HttpError, e:
+      if e.resp.status in RETRIABLE_STATUS_CODES:
+        error = "A retriable HTTP error %d occurred:\n%s" % (e.resp.status,
+                                                             e.content)
+      else:
+        raise
+    except RETRIABLE_EXCEPTIONS, e:
+      error = "A retriable error occurred: %s" % e
+
+    if error is not None:
+      print error
+      retry += 1
+      if retry > MAX_RETRIES:
+        app.logger.info("MAX_RETRIES :(")
+        return
+
+      max_sleep = 2 ** retry
+      sleep_seconds = rand.random() * max_sleep
+      print "Sleeping %f seconds and then retrying..." % sleep_seconds
+      time.sleep(sleep_seconds)
 
 app.config["UPLOAD_FOLDER"] = ""
 # These are the extension that we are accepting to be uploaded
@@ -50,84 +104,105 @@ if os.environ.get("HEROKU") is not None:
   app.logger.info("starting app")
 
 flow = OAuth2WebServerFlow(client_id=app.config["GOOGLE_CLIENT_ID"],
-						   client_secret=app.config["GOOGLE_CLIENT_SECRET"],
-						   scope=app.config["GOOGLE_API_SCOPE"])
+                           client_secret=app.config["GOOGLE_CLIENT_SECRET"],
+                           scope=app.config["GOOGLE_API_SCOPE"])
 user_info_service = build("oauth2", "v2")
 youtube_service = build("youtube", "v3")
-						   
+    
+def get_auth_http():
+    credentials = session["credentials"]
+    return credentials.authorize(httplib2.Http())
+
 # Views
 @app.route("/")
 def index():
-	if "credentials" in session:
-		credentials = session["credentials"]
-		http = credentials.authorize(httplib2.Http())
-		result = youtube_service.channels().list(part="snippet", mine="true").execute(http=http)
-		return render_template("index.html", channels=result["items"][0])
-	return redirect(url_for("google_login"))
+    if "credentials" in session:
+        result = youtube_service.channels().list(part="snippet", mine="true").execute(http=get_auth_http())
+        return render_template("index.html", channels=result["items"][0])
+    return redirect(url_for("google_login"))
 
 
 @app.route("/login/google")
 def google_login():
-	# CSRF
-	session["state"] = "".join(rand.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
-	flow.redirect_uri=url_for("authorized", _external=True)
-	auth_uri = flow.step1_get_authorize_url()
-	return redirect(auth_uri + "&state=%s" % (session["state"],))
+    # CSRF
+    session["state"] = "".join(rand.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
+    flow.redirect_uri=url_for("authorized", _external=True)
+    auth_uri = flow.step1_get_authorize_url()
+    return redirect(auth_uri + "&state=%s" % (session["state"],))
 
 
 @app.route("/upload-video", methods=["POST"])
 def upload():
     file = request.files["file"]
     # Check if the file is one of the allowed types/extensions
-    if file and allowed_file(file.filename):
-        # Make the filename safe, remove unsupported chars
-        filename = secure_filename(file.filename)
-        # Move the file form the temporal folder to
-        # the upload folder we setup
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(file_path)
-        base, ext = os.path.splitext(filename)
-        output_path = os.path.join("%s_out%s" % (base, ext))
-        code = sp.call(["ffmpeg", "-i", file_path, "-i", "http://s3-us-west-2.amazonaws.com/test.co/jingiel_bacterion_v2.mp3",
-         "-map", "0:1", "-map", "1:0", "-codec", "copy", "-y", output_path])
-        if code:
-        	return "error"
-        return "OK"
-    else:
-    	return "wrong file format. Supportet formats %s" % app.config["ALLOWED_EXTENSIONS"]
+    if not file or not allowed_file(file.filename):
+        return "wrong file format. Supportet formats %s" % app.config["ALLOWED_EXTENSIONS"]
+    # Make the filename safe, remove unsupported chars
+    filename = secure_filename(file.filename)
+    # Move the file form the temporal folder to
+    # the upload folder we setup
+    app.logger.info("Uploaded %s", filename)
+    file_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+    file.save(file_path)
+    base, ext = os.path.splitext(filename)
+    output_path = os.path.join("%s_out%s" % (base, ext))
+    code = sp.call(["ffmpeg", "-i", file_path, "-i", "http://s3-us-west-2.amazonaws.com/test.co/jingiel_bacterion_v2.mp3",
+     "-map", "0:1", "-map", "1:0", "-codec", "copy", "-y", output_path])
+    if code:
+        return "error"
+    insert_request = youtube_service.videos().insert(
+        part="snippet,status",
+        body=dict(
+          snippet=dict(
+            title=filename,
+            description="Trailer. Music provided by soundly.io",
+            tags=["soundly"],
+            categoryId="20" # seems to be gaming
+          ),
+          status = dict(
+            privacyStatus="unlisted"
+          )
+        ),
+        media_body=MediaFileUpload(output_path, chunksize=-1, resumable=True)
+    )
+    insert_request.http = get_auth_http()
+    app.logger.info("Started uploading processed file %s", output_path)
+    insert_request.__dict__
+    #resumable_upload(insert_request)
+    return "Video is publish as unlisted"     
 
 
 @app.route("/login/google/oauthcallback")
 def authorized():
-	# CSRF
-	if request.args["state"] != session["state"]:
-		abort(400)
-	del session["state"]
-	code = request.args.get("code")
-	if not code:
-		abort(403)
-	try:
-		credentials = flow.step2_exchange(code)
-	except FlowExchangeError:
-		abort(400)
-	g_user_id = credentials.id_token["sub"]
-	user = user_datastore.find_user(google_user_id=g_user_id)
-	if not user:
-		http = credentials.authorize(httplib2.Http())
-		result = user_info_service.userinfo().get().execute(http=http)
-		user = user_datastore.create_user(email=result["email"], active=True)
-		user.google_user_id = g_user_id
-		user.profile_url = result.get("link")
-		user.image_url = result.get("picture")
-		user.refresh_token = credentials.refresh_token
-		db.session.commit()
-	# refresh token is send only once, need to for auto token refreshing
-	credentials.refresh_token = user.refresh_token
-	session["credentials"] = credentials
-	return redirect(url_for("index"))
+    # CSRF
+    if request.args["state"] != session["state"]:
+        abort(400)
+    del session["state"]
+    code = request.args.get("code")
+    if not code:
+        abort(403)
+    try:
+        credentials = flow.step2_exchange(code)
+    except FlowExchangeError:
+        abort(400)
+    g_user_id = credentials.id_token["sub"]
+    user = user_datastore.find_user(google_user_id=g_user_id)
+    if not user:
+        http = credentials.authorize(httplib2.Http())
+        result = user_info_service.userinfo().get().execute(http=http)
+        user = user_datastore.create_user(email=result["email"], active=True)
+        user.google_user_id = g_user_id
+        user.profile_url = result.get("link")
+        user.image_url = result.get("picture")
+        user.refresh_token = credentials.refresh_token
+        db.session.commit()
+    # refresh token is send only once, need to for auto token refreshing
+    credentials.refresh_token = user.refresh_token
+    session["credentials"] = credentials
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
-	if os.environ.get("DEBUG") is not None:
-		app.config["DEBUG"] = True
-	app.run()
+    if os.environ.get("DEBUG") is not None:
+        app.config["DEBUG"] = True
+    app.run()
