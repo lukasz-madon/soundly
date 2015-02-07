970a1637c0a187d18a121f10993c3068e267f711
lukas madon <lukas@firestartr.co>
2015-02-07 17:16:45 +0000

    Change build ffmpeg to disable asm

--- build_ffmpeg.sh
+++ build_ffmpeg.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 git clone --depth 1 git://source.ffmpeg.org/ffmpeg
 cd ffmpeg
-./configure
+# disable-asm cause possible slow down (not sure if affects current usage)
+# TODO investigate if needed. Include yasm as dll?
+./configure --enable-shared --disable-asm --prefix=/app/vendor/ffmpeg
 make
 make install
 tar xvf ffmpeg.tgz
