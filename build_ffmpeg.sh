ad8cae4282fbdfb54207afad2a99bad2287cfa73
lukas madon <lukas@firestartr.co>
2015-02-07 18:48:38 +0000

    Add compile for ffmpeg

--- build_ffmpeg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git clone --depth 1 git://source.ffmpeg.org/ffmpeg
-cd ffmpeg
-# disable-asm cause possible slow down (not sure if affects current usage)
-# TODO investigate if needed. Include yasm as dll?
-./configure --enable-shared --disable-asm --prefix=/app/vendor/ffmpeg
-make
-make install
-tar xvf ffmpeg.tgz
