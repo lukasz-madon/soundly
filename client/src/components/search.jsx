deb46e5056641ad90cdd16aea6d4b1e6832ccf68
lukas madon <lukas@firestartr.co>
2015-05-03 12:51:08 +0100

    Add model for video list
    - add extra search tile

--- client/src/components/search.jsx
+++ client/src/components/search.jsx
@@ -6,20 +6,26 @@ var Search = React.createClass({
   render: function() {
     var hits = [{ url: 'foo.com',
                   id: 1,
-                  _highlightResult: { 
+                  _highlightResult: {
                     artist: { value: 'fooart' }, title : { value: 'footitle' }, tag : { value: 'tagfoo' }
                   }
                 },
                 { url: 'bar.com',
                   id: 1,
-                  _highlightResult: { 
+                  _highlightResult: {
                     artist: { value: 'barart' }, title : { value: 'bartitle' }, tag : { value: 'tagbar' }
                   }
                 },
                 { url: 'baz.com',
                   id: 1,
-                  _highlightResult: { 
+                  _highlightResult: {
                     artist: { value: 'bazart' }, title : { value: 'baztitle' }, tag : { value: 'tagbaz' }
+                  }
+                },
+                { url: 'bax.com',
+                  id: 1,
+                  _highlightResult: {
+                    artist: { value: 'baxart' }, title : { value: 'baztitle' }, tag : { value: 'tagbaz' }
                   }
                 }];
     var items = hits.map(function(hit) {
--- client/src/components/videolist.jsx
+++ client/src/components/videolist.jsx
@@ -3,17 +3,32 @@ var mui = require('material-ui');
 
 var VideoList = React.createClass({
   render: function() {
+    var foo = [{
+                id: 'pXEN57rFnIM',
+                title: 'Samantha Fox - Naughty Girls Need Love Too',
+                description: 'Music video by Samantha Fox performing Naughty Girls Need Love Too. (C) 1988 Zomba Records Limited',
+              },
+              {
+                id: 'TS-G4UQTfUo',
+                title: 'Caravan',
+                description: ''
+              }];
+    var videos = foo.map(function(video, index){
+      return (
+        <tr>
+          <td>{index + 1}</td>
+          <td className="video-link" data-id={video.id} data-description={video.description}>
+            {video.title}
+          </td>
+        </tr>
+      );
+    });
     return (
       <div>
         <h4>Pick Youtube Video</h4>
           <table className="table table-striped table-hover ">
             <tbody>
-              <tr>
-                <td>loop.index</td>
-                <td className="video-link" data-id="video.id" data-description="video.description ">
-                  video.title
-                </td>
-              </tr>
+              {videos}
             </tbody>
           </table>
       </div>
