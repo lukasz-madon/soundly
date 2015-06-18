fab9e103152c9110ba46f9deb5ccfaafd746ac12
lukas madon <lukasz.madon@gmail.com>
2015-06-18 22:46:08 +0100

    Add ajax call for processing video

--- client/src/components/publishform.jsx
+++ client/src/components/publishform.jsx
@@ -1,7 +1,35 @@
 import mui from 'material-ui';
 import React from 'react';
+import FluxComponent from 'flummox/component';
+
 
 let PublishFrom = React.createClass({
+  render: function() {
+    return (
+      <FluxComponent connectToStores={['video', 'videoMeta']}>
+        <PublishFromInner />
+      </FluxComponent>
+    );
+  }
+});
+
+let PublishFromInner = React.createClass({
+  handlePublish: function() {
+    let newVideo = {
+      audioVolume: this.props.audioVolume,
+      currentVideo: this.props.currentVideo,
+      description: this.props.description,
+      musicUrl: this.props.musicUrl,
+      musicVolume: this.props.musicVolume,
+      privacyStatus : this.props.privacyStatus,
+      title: this.props.title
+    };
+    $.ajax('process-video', {
+      data : JSON.stringify(newVideo),
+      contentType : 'application/json',
+      type : 'POST'
+    });
+  },
   render: function() {
     let menuItems = [
       { payload: '1', text: 'Public' },
@@ -23,7 +51,7 @@ let PublishFrom = React.createClass({
          </div>
          <div className="row">
           <div className="col-xs-5">
-            <mui.RaisedButton className="publish-btn" label="Publish" primary={true}/>
+            <mui.RaisedButton className="publish-btn" label="Publish" primary={true} onClick={this.handlePublish}/>
            </div>
           <div className="col-xs-6">
             <mui.DropDownMenu menuItems={menuItems} />
